d61dd2d1156fd592fe07ab3f440f6ba08a9f28da
Patricio Campos <patricio.campos@userena.cl>
2023-02-17 14:32:30 -0300

    Este es el primer commit para la pagina de forza training

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function izquierda() {
+    document.querySelector('.carr').scrollLeft = document.querySelector('.carr').scrollLeft - 300;
+}
+
+function derecha() {
+    document.querySelector('.carr').scrollLeft = document.querySelector('.carr').scrollLeft + 300;
+}
