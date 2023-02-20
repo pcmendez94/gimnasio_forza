b454844dff3c623f144b8b5302daaa3339fdb0ea
Patricio Campos <patricio.campos@userena.cl>
2023-02-20 16:22:07 -0300

    Se hace actualizacion de página forza training 20-02-2023

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,25 @@
+var ancho_carrousel_item = 300;
+
 function izquierda() {
-    document.querySelector('.carr').scrollLeft = document.querySelector('.carr').scrollLeft - 300;
+    document.querySelector('.carr-inner').scrollLeft = document.querySelector('.carr-inner').scrollLeft - ancho_carrousel_item;
 }
 
 function derecha() {
-    document.querySelector('.carr').scrollLeft = document.querySelector('.carr').scrollLeft + 300;
+    document.querySelector('.carr-inner').scrollLeft = document.querySelector('.carr-inner').scrollLeft + ancho_carrousel_item;
+}
+
+window.addEventListener('resize', () =>{
+    detecta_ancho_pantalla();
+});
+
+window.addEventListener('load', () =>{
+    detecta_ancho_pantalla();
+});
+
+function detecta_ancho_pantalla() {
+    if (window.innerWidth < 576) {
+        ancho_carrousel_item = 200;
+    } else {
+        ancho_carrousel_item = 300;
+    }
 }
